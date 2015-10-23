5c762c6f51966a8396dd344e596e773b455ab6a0
bokuweb <info@typebeats.com>
2015-10-23 13:06:47 +0900

    add compact & differnce

--- lib/edash.ex
+++ lib/edash.ex
@@ -1,9 +1,26 @@
 defmodule Edash do
-  def one do
-    1
+  def chunk([h|t]), do: [[h],t]
+  def chunk(list, size), do: chunk(list, size, [], [])
+  def chunk(_, 0, acc1, acc2), do: [acc1, acc2]
+  def chunk([], _, acc1, acc2), do: [acc1, acc2]
+  def chunk([h|t], size, acc1, acc2) do
+    if size >= 0, do: chunk(t, size-1, acc1 ++ [h], t)
+    else []
   end
 
-  defp onep do
-    1
+  def compact(list) do
+    Enum.filter list, fn x ->
+      x !== false and
+      x !== ""    and
+      x !== ''    and
+      x !== nil   and
+      x !== 0
+    end
+  end
+
+  def difference(list, values) do
+    Enum.filter list, fn l ->
+      Enum.all? values, fn v -> l !== v end
+    end
   end
 end
--- test/edash_test.exs
+++ test/edash_test.exs
@@ -2,7 +2,11 @@ defmodule EdashTest do
   use ExUnit.Case
   doctest Edash
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "compact/1" do
+    assert(Edash.compact([0, 1, false, 2, '', "", 3]) === [1, 2, 3])
+  end
+
+  test "difference/2" do
+    assert(Edash.difference([1, 2, 3], [4, 2]) === [1, 3])
   end
 end
